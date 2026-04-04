954c398579a8440da39f9b48b4461aff8a11aeb2
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-04 15:32:14 +0100

    vai

--- affable_web/app/components/login-form.tsx
+++ affable_web/app/components/login-form.tsx
@@ -17,7 +17,7 @@ export function LoginForm({
     <form className={cn("flex flex-col gap-6", className)} {...props}>
       <FieldGroup>
         <div className="flex flex-col items-center gap-1 text-center">
-          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <h1 className="text-xl font-bold">Login to your account</h1>
           <p className="text-xs text-balance text-muted-foreground">
             Enter your email below to login to your account
           </p>
