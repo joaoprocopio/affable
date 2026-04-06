ddcbfbfb942cb61063640a7dedcb8c8f606f6bde
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-06 02:55:03 +0100

    vai

---
+++ apps/core/app/Http/Requests/SignInRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Email;
+use Illuminate\Validation\Rules\Password;
+
+class SignInRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', Email::default()],
+            'password' => ['required', Password::defaults()],
+        ];
+    }
+}
