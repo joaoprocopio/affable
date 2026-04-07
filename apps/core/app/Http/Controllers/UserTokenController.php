8dfd1eef8da6113f68f7447c0451f2777e9914a1
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-07 04:16:30 +0100

    vai

--- apps/core/app/Http/Controllers/UserTokenController.php
+++ apps/core/app/Http/Controllers/UserTokenController.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
 final class UserTokenController extends Controller
 {
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(Request $request): Response
     {
-        return new JsonResponse($request->session()->token(), status: JsonResponse::HTTP_OK);
+        $request->session()->token();
+
+        return new Response(status: Response::HTTP_OK);
     }
 }
