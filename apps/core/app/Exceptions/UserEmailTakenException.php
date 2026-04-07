44b1949b78fbe8dbfb4974e756c13952c99a33c8
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-07 03:54:13 +0100

    vai

--- apps/core/app/Exceptions/UserEmailTakenException.php
+++ apps/core/app/Exceptions/UserEmailTakenException.php
@@ -4,14 +4,13 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class UserEmailTakenException extends Exception
 {
     /**
      * Render the exception as an HTTP response.
      */
-    public function render(Request $request): JsonResponse
+    public function render(): JsonResponse
     {
         return new JsonResponse(["code" => "EMAIL_ALREADY_TAKEN"], status: JsonResponse::HTTP_CONFLICT);
     }
