86a69c401922f1f6de2dbd0d4b1f1329551cc4d5
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-07 04:25:17 +0100

    vai

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,6 +1,11 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from "@react-router/dev/config"
 
 export default {
   ssr: false,
   prerender: true,
-} satisfies Config;
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+  },
+} satisfies Config
