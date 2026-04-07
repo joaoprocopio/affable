319b09f23b61c7ddb90bb2858f28afee07f4f605
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-07 06:15:34 +0100

    vai

--- apps/web/app/routes/app.tsx
+++ apps/web/app/routes/app.tsx
@@ -1,5 +1,14 @@
 import { Outlet } from "react-router"
+import { Sidebar, SidebarInset, SidebarProvider } from "~/lib/ui/sidebar"
 
 export default function AppRoute() {
-  return <Outlet />
+  return (
+    <SidebarProvider>
+      <Sidebar variant="sidebar"></Sidebar>
+
+      <SidebarInset>
+        <Outlet />
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
