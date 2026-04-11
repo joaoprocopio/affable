4834aa9514b2d084b2fb9c0d1d54377fd799ddf0
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-11 18:17:58 +0100

    vaig

--- apps/web/app/routes/properties-add.tsx
+++ apps/web/app/routes/properties-add.tsx
@@ -1,5 +1,12 @@
 import { AppHeader, AppHeaderBreadcrumb } from "~/components/app-header"
-import { Field, FieldGroup, FieldLabel } from "~/lib/ui/field"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from "~/lib/ui/field"
 import { Input } from "~/lib/ui/input"
 import { Textarea } from "~/lib/ui/textarea"
 
@@ -16,18 +23,78 @@ export default function PropertiesAddRoute() {
         <AppHeaderBreadcrumb />
       </AppHeader>
 
-      <div className="mx-auto max-w-2xl px-6 py-4">
-        <FieldGroup>
-          <Field>
-            <FieldLabel>Property name</FieldLabel>
-            <Input />
-          </Field>
+      <div className="mx-auto max-w-2xl px-6 py-8">
+        <form className="flex flex-col gap-12">
+          <FieldSet>
+            <FieldLegend>About</FieldLegend>
+            <FieldDescription>
+              This information will help you easily identify your property inside the platform.
+            </FieldDescription>
 
-          <Field>
-            <FieldLabel>Description</FieldLabel>
-            <Textarea />
-          </Field>
-        </FieldGroup>
+            <FieldGroup>
+              <Field>
+                <FieldLabel>Name</FieldLabel>
+                <Input />
+              </Field>
+
+              <Field>
+                <FieldLabel>Description</FieldLabel>
+                <Textarea />
+              </Field>
+            </FieldGroup>
+          </FieldSet>
+
+          <FieldSet>
+            <FieldLegend>Address</FieldLegend>
+            <FieldDescription>asd</FieldDescription>
+
+            <FieldGroup>
+              <Field>
+                <FieldLabel>Country</FieldLabel>
+                <Input />
+              </Field>
+
+              <Field>
+                <FieldLabel>City</FieldLabel>
+                <Input />
+              </Field>
+
+              <Field>
+                <FieldLabel>
+                  State <FieldDescription>(if applicable)</FieldDescription>
+                </FieldLabel>
+                <Input />
+              </Field>
+
+              <Field>
+                <FieldLabel>
+                  Postal code
+                  <FieldDescription>(if applicable)</FieldDescription>
+                </FieldLabel>
+                <Input />
+              </Field>
+
+              <Field>
+                <FieldLabel>Address line 1</FieldLabel>
+                <Input />
+              </Field>
+
+              <Field>
+                <FieldLabel>
+                  Address line 2 <FieldDescription>(if applicable)</FieldDescription>
+                </FieldLabel>
+                <Input />
+              </Field>
+
+              <Field>
+                <FieldLabel>
+                  Address line 3 <FieldDescription>(if applicable)</FieldDescription>
+                </FieldLabel>
+                <Input />
+              </Field>
+            </FieldGroup>
+          </FieldSet>
+        </form>
       </div>
     </div>
   )
