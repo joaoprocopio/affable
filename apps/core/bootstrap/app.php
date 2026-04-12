589c9c76849f62d1ce7fb324761152ed3a0a55d7
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-12 02:12:07 +0100

    vai

--- apps/core/bootstrap/app.php
+++ apps/core/bootstrap/app.php
@@ -10,9 +10,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Middleware\ValidatePostSize;
 use Illuminate\Http\Middleware\ValidatePathEncoding;
@@ -33,16 +31,24 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->api([
             HandleCors::class,
+
+            // validation
             ValidatePostSize::class,
             ValidatePathEncoding::class,
+
+            // normalization
             TrimStrings::class,
             ConvertEmptyStringsToNull::class,
+
+            // auth
             EncryptCookies::class,
             AddQueuedCookiesToResponse::class,
             StartSession::class,
-            AuthenticateSession::class,
+
+            // csrf
             PreventRequestForgery::class,
-            ThrottleRequests::class,
+
+            // dependency injection
             SubstituteBindings::class,
         ]);
     })
