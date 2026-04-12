ec26d7cbe3b2ae250196ab588f543d892be14391
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-12 03:10:45 +0100

    vai

--- apps/core/app/Http/Controllers/PropertiesListController.php
+++ apps/core/app/Http/Controllers/PropertiesListController.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PropertyAddRequest;
 use App\Http\Resources\PropertyResource;
 use App\Property;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 final class PropertiesListController extends Controller
 {
-    public function __invoke(PropertyAddRequest $request): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
         $user_id = $request->user()->id;
-        $properties = Property::query()->where('user_id', $user_id);
+        $properties = Property::query()->where('user_id', $user_id)->get();
 
-        return new JsonResponse(new PropertyResource($properties), JsonResponse::HTTP_OK);
+        return new JsonResponse(PropertyResource::collection($properties), JsonResponse::HTTP_OK);
     }
 }
