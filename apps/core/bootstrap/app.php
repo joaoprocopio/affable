5e14777002ee4dd4afbf3849156daf4f2021e8e1
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-16 23:59:15 +0100

    vai

--- apps/core/bootstrap/app.php
+++ apps/core/bootstrap/app.php
@@ -11,7 +11,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Middleware\ValidatePostSize;
@@ -30,7 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
             ForceJsonMiddleware::class,
         ]);
 
-        $middleware->redirectGuestsTo(fn () => null);
+        $middleware->redirectGuestsTo(fn() => null);
 
         $middleware->api([
             // cors
@@ -50,7 +49,6 @@ return Application::configure(basePath: dirname(__DIR__))
             // session, auth
             AddQueuedCookiesToResponse::class,
             StartSession::class,
-            AuthenticateSession::class,
 
             // csrf
             PreventRequestForgery::class,
