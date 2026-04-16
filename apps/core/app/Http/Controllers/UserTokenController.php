1bf5f485f7f02ee3bc2b0559b481f036d7c913bf
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-16 23:51:04 +0100

    vai

--- apps/core/app/Http/Controllers/UserTokenController.php
+++ apps/core/app/Http/Controllers/UserTokenController.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
 final class UserTokenController extends Controller
 {
-    public function __invoke(Request $request): Response
+    public function __invoke(): Response
     {
         csrf_token();
+
         return new Response(status: Response::HTTP_OK);
     }
 }
